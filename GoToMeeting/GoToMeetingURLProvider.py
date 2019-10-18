c9a42ef6197517ef3cd3caf3671781c85251f09f
karthik selvakumar <karthikselvakumar7@gmail.com>
2019-10-18 19:59:44 +0530

    Update GoToMeetingURLProvider.py

--- GoToMeeting/GoToMeetingURLProvider.py
+++ GoToMeeting/GoToMeetingURLProvider.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2015 Elliot Jordan
-# Based on original processor by Nick Gamewell
+# Copyright 2014 Nick Gamewell
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,23 +14,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import gzip
+from __future__ import absolute_import
+
 import json
-import re
-import StringIO
-import urllib2
 
 from autopkglib import Processor, ProcessorError
 
+try:
+    from urllib.parse import urlopen  # For Python 3
+except ImportError:
+    from urllib2 import urlopen  # For Python 2
 
 __all__ = ["GoToMeetingURLProvider"]
 
 
-BASE_URL = "https://p5.osdimg.com/g2mupdater/live/config.json"
+BASE_URL = "https://builds.cdn.getgo.com/g2mupdater/live/config.json"
 
 
 class GoToMeetingURLProvider(Processor):
-
     """Provides a download URL for the latest GoToMeeting release."""
     input_variables = {
         "base_url": {
@@ -49,39 +49,27 @@ class GoToMeetingURLProvider(Processor):
     }
     description = __doc__
 
-    def get_g2m_json(self, base_url):
-        request = urllib2.Request(base_url)
-        request.add_header("Accept-Encoding", "gzip")
-        opener = urllib2.build_opener()
-        response = opener.open(request)
+    def get_g2m_url(self, base_url):
+        try:
+            jsonData = json.loads(urlopen(base_url).read())
+            return jsonData['activeBuilds'][len(jsonData['activeBuilds'])-1]['macDownloadUrl']
+        except Exception as err:
+            raise Exception("Can't read %s: %s" % (base_url, err))
 
-        # Sometimes the base URL is compressed as gzip, sometimes it's not.
-        if response.info().get("Content-Encoding") == "gzip":
-            self.output("Encoding: gzip")
-            gzipData = StringIO.StringIO(response.read())
-            jsonData = json.loads(gzip.GzipFile(fileobj=gzipData).read())
-        else:
-            self.output("Encoding: plaintext")
-            jsonData = json.loads(response.read())
-
-        return jsonData
-
-    def get_g2m_url(self, jsonData):
-        return jsonData["activeBuilds"][
-            len(jsonData["activeBuilds"]) - 1]["macDownloadUrl"]
-
-    def get_g2m_build(self, jsonData):
-        return str(jsonData["activeBuilds"][
-            len(jsonData["activeBuilds"]) - 1]["buildNumber"])
+    def get_g2m_build(self, base_url):
+        try:
+            jsonData = json.loads(urlopen(base_url).read())
+            return str(jsonData['activeBuilds'][len(jsonData['activeBuilds'])-1]['buildNumber'])
+        except Exception as err:
+            raise Exception("Can't read %s: %s" % (base_url, err))
 
     def main(self):
         """Find and return a download URL"""
         base_url = self.env.get("base_url", BASE_URL)
-        jsonData = self.get_g2m_json(base_url)
-        self.env["url"] = self.get_g2m_url(jsonData)
-        self.output("Found URL: %s" % self.env["url"])
-        self.env["build"] = self.get_g2m_build(jsonData)
-        self.output("Build number: %s" % self.env["build"])
+        self.env["url"] = self.get_g2m_url(base_url)
+        self.output("Found URL %s" % self.env["url"])
+        self.env["build"] = self.get_g2m_build(base_url)
+        self.output("Build number %s" % self.env["build"])
 
 if __name__ == "__main__":
     processor = GoToMeetingURLProvider()
